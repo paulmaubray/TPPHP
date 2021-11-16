43b2fabf83595f1cf659063a9f0dc60fc404c140
paulmaubray <paul.maubray@gmail.com>
2021-11-16 11:06:57 +0100

    Third commit

---
+++ tableau3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<?php
+         $marque = array("volvo","BMW","Toyota", "Peugeot","Renault",);
+         $i=0;
+         foreach ($elem as $marque) {
+               echo "Vente de : " . $elem . "<br/>";
+} ?>
+
+ </body>
+</html>
